20bd94f06186617ff88e49a274db47b5e971d506
ZainSaleem1110 <zainsaleem6625@gmail.com>
2022-08-27 01:39:52 +0500

    complete

--- src/App.js
+++ src/App.js
@@ -8,28 +8,28 @@ function App() {
     return (
         <>
             <Box className="flex justify-center items-center mb-3 text-[#825AFF]">
-                <Typography sx={{ fontSize: "42px" }}>JOIN THE WHITELIST</Typography>
+                <Typography><span className="sm:text-[42px] text-[20px]">JOIN THE WHITELIST</span></Typography>
             </Box>
             <Box className="w-[100%] flex">
-                <Box className="mt-3">
+                <Box className="mt-3 md:flex hidden items-start">
                     <img src={DotImg1} className="w-[70px] pr-3" alt="" />
                 </Box>
-                <Box className="w-[600px] h-[80vh] bg-[#161719] flex justify-center items-center">
-                    <Box className="w-[350px]">
+                <Box className="sm:w-[600px] sm:px-0 px-10 py-10 bg-[#161719] flex justify-center items-center">
+                    <Box className="sm:w-[350px] w-[250px]">
                         <Box>
-                            <label className="text-white pl-3">Name</label>
+                            <label className="text-white pl-3 block">Name</label>
                             <input className="bg-black mt-1 pl-3 text-white h-[42px] w-[100%] rounded-sm" name="lastName" required placeholder="Enter your full name" />
                         </Box>
                         <Box className="mt-4">
-                            <label className="text-white pl-3">Email</label>
+                            <label className="text-white pl-3 block">Email</label>
                             <input className="bg-black mt-1 pl-3 text-white h-[42px] w-[100%] rounded-sm" type="email" name="Email" required placeholder="Enter your email address" />
                         </Box>
                         <Box className="mt-4">
-                            <label className="text-white pl-3">Wallet Address</label>
+                            <label className="text-white pl-3 block">Wallet Address</label>
                             <input className="bg-black mt-1 pl-3 text-white h-[42px] w-[100%] rounded-sm" type="email" name="Email" required placeholder="Enter your email address" />
                         </Box>
                         <Box className="mt-4">
-                            <label className="text-white pl-3">Wallet Address</label>
+                            <label className="text-white pl-3 block">Wallet Address</label>
                             <input className="bg-black mt-1 pl-3 text-white h-[42px] w-[100%] rounded-sm" type="email" name="Email" required placeholder="Enter your email address" />
                         </Box>
                         <Box className="flex justify-center mt-4">
@@ -39,8 +39,8 @@ function App() {
                         </Box>
                     </Box>
                 </Box>
-                <Box className="mt-5 ml-5 flex item-center">
-                    <img src={DotImg2} className="w-[70px] pl-3" alt="" />
+                <Box className="mt-5 ml-5 hidden md:flex item-center">
+                    <img src={DotImg2} className="w-[70px] pl-3 md:flex hidden" alt="" />
                 </Box>
             </Box>
         </>
