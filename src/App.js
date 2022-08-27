ce31b84ec98917d1885a5a9d1edc36d5d647e308
ZainSaleem1110 <zainsaleem6625@gmail.com>
2022-08-27 15:21:57 +0500

    change border color

--- src/App.js
+++ src/App.js
@@ -7,34 +7,34 @@ import DotImg2 from './images/dots.svg'
 function App() {
     return (
         <>
-            <Box className="flex justify-center items-center mb-3 text-[#825AFF] relative">
+            <Box className="flex justify-center items-center mb-3 text-[#825AFF] relative ">
                 <Typography><span className="sm:text-[42px] text-[20px] heading">JOIN THE WHITELIST</span></Typography>
-                <Box className="absolute top-0 right-20">
-                    <Box className="h-[25px] w-[5px] bg-[#825AFF] rounded-lg rotate-45"></Box>
-                    <Box className="h-[14px] w-[5px] bg-white rounded-lg -rotate-45 mt-2"></Box>
+                <Box className="absolute top-0 md:right-20 right-0">
+                    <Box className="sm:h-[25px] h-[15px] w-[5px] bg-[#825AFF] rounded-lg rotate-45"></Box>
+                    <Box className="sm:h-[14px] h-[8px] w-[5px] bg-white rounded-lg -rotate-45 mt-2"></Box>
                 </Box>
             </Box>
             <Box className="w-[100%] flex">
                 <Box className="mt-3 md:flex hidden items-start">
                     <img src={DotImg1} className="w-[70px] pr-3" alt="" />
                 </Box>
-                <Box className="sm:w-[600px] sm:px-0 px-10 py-10 bg-[#161719] flex justify-center items-center">
+                <Box className="sm:w-[600px] w-[100%] sm:px-0 px-10 py-10 bg-[#161719] flex justify-center items-center">
                     <Box className="sm:w-[350px] w-[250px]">
                         <Box>
                             <label className="text-white pl-3 block">Name</label>
-                            <input className="bg-black mt-1 pl-3 text-white h-[42px] w-[100%] rounded-sm text-[14px] focus:outline-none focus:ring focus:border-[#825AFF]" name="lastName" required placeholder="Enter your full name" />
+                            <input className="bg-black mt-1 pl-3 text-white h-[42px] w-[100%] rounded-sm text-[14px] focus:outline-[#825AFF] focus:border-none border-none outline-none" name="lastName" required placeholder="Enter your full name" />
                         </Box>
                         <Box className="mt-4">
                             <label className="text-white pl-3 block">Email</label>
-                            <input className="bg-black mt-1 pl-3 text-white text-[14px] h-[42px] w-[100%] rounded-sm focus:outline-none focus:ring focus:border-[#825AFF]" type="email" name="Email" required placeholder="Enter your email address" />
+                            <input className="bg-black mt-1 pl-3 text-white text-[14px] h-[42px] w-[100%] rounded-sm focus:outline-[#825AFF] focus:border-none border-none outline-none" type="email" name="Email" required placeholder="Enter your email address" />
                         </Box>
                         <Box className="mt-4">
                             <label className="text-white pl-3 block">Wallet Address</label>
-                            <input className="bg-black mt-1 text-[14px] pl-3 text-white h-[42px] w-[100%] rounded-sm focus:outline-none focus:ring focus:border-[#825AFF]" type="email" name="Email" required placeholder="Enter your email address" />
+                            <input className="bg-black mt-1 text-[14px] pl-3 text-white h-[42px] w-[100%] rounded-sm focus:outline-[#825AFF] focus:border-none border-none outline-none" type="email" name="Email" required placeholder="Enter your email address" />
                         </Box>
                         <Box className="mt-4">
                             <label className="text-white pl-3 block">Wallet Address</label>
-                            <input className="bg-black mt-1 text-[14px] pl-3 text-white h-[42px] w-[100%] rounded-sm focus:outline-none focus:ring focus:border-[#825AFF]" type="email" name="Email" required placeholder="Enter your email address" />
+                            <input className="bg-black mt-1 text-[14px] pl-3 text-white h-[42px] w-[100%] rounded-sm focus:outline-[#825AFF] focus:border-none border-none outline-none" type="email" name="Email" required placeholder="Enter your email address" />
                         </Box>
                         <Box className="flex justify-center mt-4">
                             <button className="w-[180px] h-[42px] text-[#46D7D7] hover:bg-[#46D7D7] hover:text-white" style={{border:"1px solid #46D7D7"}}>
